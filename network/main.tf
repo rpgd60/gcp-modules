e02fd18179d692a21ed05caf4f016eb16c203e85
Rafa <rafa@portillo.eu>
2023-05-21 20:16:16 +0200

    clean up firewall rules

--- network/main.tf
+++ network/main.tf
@@ -17,28 +17,7 @@ resource "google_compute_subnetwork" "mi_subnet" {
 
 ## Firewall rules
 
-
-resource "google_compute_firewall" "iap" {
-  count = var.num_vpcs
-  name  = "${local.name_prefix}-iap-${count.index}"
-  allow {
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.mi_vpc.id
-  priority      = 1000
-  source_ranges = [var.iap_range]
-  target_tags   = ["ssh"]
-  dynamic  log_config {
-    for_each = var.enable_fw_logging ? ["dummy"] : []
-    content {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }
-}
-
 resource "google_compute_firewall" "ssh" {
-  count = var.num_vpcs
   name  = "${local.name_prefix}-ssh-${count.index}"
   allow {
     ports    = ["22"]
@@ -57,25 +36,6 @@ resource "google_compute_firewall" "ssh" {
   }
 }
 
-resource "google_compute_firewall" "ssh_v6" {
-  count = var.num_vpcs
-  name  = "${local.name_prefix}-ssh-v6-${count.index}"
-  allow {
-    ports    = ["22"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc[count.index].id
-  priority      = var.def_fw_rule_priority
-  source_ranges = ["::/0"]
-  target_tags   = ["ssh"]
-  dynamic  log_config {
-    for_each = var.enable_fw_logging ? ["dummy"] : []
-    content {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }
-}
 resource "google_compute_firewall" "web" {
   count = var.num_vpcs
   name  = "${local.name_prefix}-web-${count.index}"
@@ -97,67 +57,6 @@ resource "google_compute_firewall" "web" {
   }
 }
 
-resource "google_compute_firewall" "app" {
-  count = var.num_vpcs
-  name  = "${local.name_prefix}-app-${count.index}"
-  allow {
-    ports    = [var.app_port]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc[count.index].id
-  priority      = var.def_fw_rule_priority
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["app"]
-
-  dynamic  log_config {
-    for_each = var.enable_fw_logging ? ["dummy"] : []
-    content {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }
-}
-
-resource "google_compute_firewall" "app_v6" {
-  count = var.num_vpcs
-  name  = "${local.name_prefix}-app-v6-${count.index}"
-  allow {
-    ports    = [var.app_port]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc[count.index].id
-  priority      = var.def_fw_rule_priority
-  source_ranges = ["::/0"]
-  target_tags   = ["app"]
-
-  dynamic  log_config {
-    for_each = var.enable_fw_logging ? ["dummy"] : []
-    content {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }
-}
-resource "google_compute_firewall" "web_v6" {
-  count = var.num_vpcs
-  name  = "${local.name_prefix}-web-v6-${count.index}"
-  allow {
-    ports    = ["80", "443"]
-    protocol = "tcp"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc[count.index].id
-  priority      = var.def_fw_rule_priority
-  source_ranges = ["::/0"]
-  target_tags   = ["web"]
-
-  dynamic  log_config {
-    for_each = var.enable_fw_logging ? ["dummy"] : []
-    content {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }
-}
 resource "google_compute_firewall" "icmp" {
   count = var.num_vpcs
   name  = "${local.name_prefix}-icmp-${count.index}"
@@ -178,22 +77,4 @@ resource "google_compute_firewall" "icmp" {
   }
 }
 
-resource "google_compute_firewall" "icmp_v6" {
-  count = var.num_vpcs
-  name  = "${local.name_prefix}-icmp-v6-${count.index}"
-  allow {
-    ## 58 is protocol for ICMP-v6 
-    protocol = "58"
-  }
-  direction     = "INGRESS"
-  network       = google_compute_network.vpc[count.index].id
-  priority      = var.def_fw_rule_priority
-  source_ranges = ["::/0"]
-  target_tags   = ["icmp"]
-  dynamic  log_config {
-    for_each = var.enable_fw_logging ? ["dummy"] : []
-    content {
-      metadata = "INCLUDE_ALL_METADATA"
-    }
-  }
-}
+
